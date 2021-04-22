66f9e2a197db53d9785d4309d91dda95a51a8938
EuricoDNJR <massaultyplay@gmail.com>
2021-04-22 13:10:54 -0300

    .

--- 1071.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-int main() {
-
-int x, y, i, z=0, c; scanf ("%d %d", &x, &y); if (x < y){ c = x; x = y; y = c; } if (x == y){ printf ("0\n"); }else{ for (i = y+1; i < x; i++) if (i%2 !=0) z = i + z; printf ("%d\n", z); }
-
-return 0; }
--- Em C/2234.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main(int argc, char** argv)
-{
-	float h,p;
-	scanf("%f %f",&h,&p);
-	printf("%.2f\n",h/p);
-	return 0;
-}
